914329981125a00dab6b7759cb4b12492001b5bd
sneha-paul <sneha.paul@e-xposures.com>
2021-11-22 16:48:37 +0530

    Added my projects

---
+++ src/test/java/com/example/test/demojenkins/DemojenkinsApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.test.demojenkins;
+
+
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+public class DemojenkinsApplicationTests {
+
+	@Test
+	public void contextLoads() throws InterruptedException {
+		//System.setProperty("webdriver.chrome.driver",  "C:\\DevStudio\\Installs\\gecko\\gecko 29\\geckodriver.exe");
+		//WebDriver driver = new FirefoxDriver();
+		//driver.get("https://www.amazon.com/");
+		//Thread.sleep(2000);
+
+		System.out.println("hii");
+
+
+
+	}
+
+}
