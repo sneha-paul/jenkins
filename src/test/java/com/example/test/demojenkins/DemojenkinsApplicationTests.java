f727536d1dd70f9a2f8cba7f004c485af968fd49
sneha-paul <sneha.paul@e-xposures.com>
2021-11-23 16:09:53 +0530

    code change-chrome path

--- src/test/java/com/example/test/demojenkins/DemojenkinsApplicationTests.java
+++ src/test/java/com/example/test/demojenkins/DemojenkinsApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.test.demojenkins;
 
 
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,8 +14,10 @@ public class DemojenkinsApplicationTests {
 	@Test
 	public void contextLoads() throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver",  "C:\\DevStudio\\Installs\\gecko\\gecko 29\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		/*System.setProperty("webdriver.chrome.driver",  "C:\\DevStudio\\Installs\\gecko\\gecko 29\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();*/
+		System.setProperty("webdriver.chrome.driver","C:\\DevStudio\\Installs\\chrome\\chrome96\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.amazon.com/");
 		Thread.sleep(2000);
 		System.out.println("hii");
